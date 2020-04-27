7cd915ba0a816f99b79c41c54e578b60bb6aacff
Fanwei <fanwei_kong@berkeley.edu>
2020-04-26 23:56:51 -0700

    Works now for single frame

---
+++ app/run_reconstruction.cpp
@@ -0,0 +1,24 @@
+//
+// Created by gaoxiang on 19-5-4.
+// modified by jianwei zhang
+//
+
+#include <gflags/gflags.h>
+#include "myslam/visual_odometry.h"
+
+DEFINE_string(config_file, "../config/default.yaml", "config file path");
+
+int main(int argc, char **argv) {
+    //google::ParseCommandLineFlags(&argc, &argv, true);
+    
+    simpleslam::VisualOdometry::Ptr vo(
+        new simpleslam::VisualOdometry(FLAGS_config_file));
+    vo->SavePose(false);
+    vo->SavePointCloud(false);
+    vo->SetRealtime(false);
+    vo->SetBuildMap(true);
+    vo->ReconstructMesh(200); 
+
+    return 0;
+}
+
